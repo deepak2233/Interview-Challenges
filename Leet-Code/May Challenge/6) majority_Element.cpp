de5df86cdfa6f3c2fd83c4f751a1d299c6f83d08
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-12 18:12:47 +0530

    Delete 6) majority_Element.cpp

--- Leet-Code/May Challenge/6) majority_Element.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int l=nums.size()-1;
-        sort(nums.begin(),nums.end());
-        return nums[l/2];    
-    }
-};
