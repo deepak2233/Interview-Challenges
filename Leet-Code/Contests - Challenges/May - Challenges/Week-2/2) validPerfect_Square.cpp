07ecdc9ead8602df0c991841e2a1ddd9a3db7880
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-27 11:06:50 +0530

    1

---
+++ Leet-Code/Contests - Challenges/May - Challenges/Week-2/2) validPerfect_Square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int sq = (sqrt(num));
+        
+        if(num==sq*sq)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+};
