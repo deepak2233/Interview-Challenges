c3794a116fa456174a569f8de68469f5e05f7f18
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-06-07 21:05:30 +0530

    1

---
+++ Leet-Code/Contests - Challenges/June - Challenges/Week-1/7) Coin_Change2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int change(int m, vector<int>& coins) {
+        int n=coins.size();
+        int t[n+1][m+1];
+        for(int i=0;i<=n;i++)
+            t[i][0]=1;
+        for(int i=1;i<=m;i++)
+            t[0][i]=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(coins[i-1]<=j)
+                    t[i][j]=t[i-1][j]+t[i][j-coins[i-1]];
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+        return t[n][m];
+    }
+};
